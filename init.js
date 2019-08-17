b6d4e0488bcc36951e2904a206695698836a1c02
Abel <abel.hancock@liferay.com>
2019-08-16 20:20:01 -0700

    add source files

---
+++ init.js
@@ -0,0 +1,24 @@
+Amplitude.init({
+	songs: [
+		{
+			"name": "Risin' High (feat Raashan Ahmad)",
+			"artist": "Ancient Astronauts",
+			"album": "We Are to Answer",
+			"url": "https://521dimensions.com/song/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
+			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+		},
+    {
+            "name": "The Gun",
+            "artist": "Lorn",
+            "album": "Ask The Dust",
+            "url": "https://521dimensions.com/song/08 The Gun.mp3",
+            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
+        },
+	],
+  playlists: {
+    "recent_work": {
+      songs: [0, 1],
+      title: 'Best of Ancient Astronauts'
+    }
+  }
+});
