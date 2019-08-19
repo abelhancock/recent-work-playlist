cbacfd841437fd8ee42f17326d597d3eff1e2ee1
Abel <abel.hancock@liferay.com>
2019-08-18 19:59:44 -0700

    more changes mannnnn

--- init.js
+++ init.js
@@ -1,13 +1,13 @@
 Amplitude.init({
   songs: [
     {
-      name: "Risin' High (feat Raashan Ahmad)",
-      artist: "Ancient Astronauts",
-      album: "We Are to Answer",
+      name: "W & G Wedding",
+      artist: "Abel Hancock",
+      album: "Recent Work",
       url:
-        "https://521dimensions.com/song/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
+        "https://github.com/abelhancock/recent-work-playlist/raw/master/audio/w-g-wedding.mp3",
       cover_art_url:
-        "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+        "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
     },
     {
       name: "The Gun",
@@ -15,13 +15,37 @@ Amplitude.init({
       album: "Ask The Dust",
       url: "https://521dimensions.com/song/08 The Gun.mp3",
       cover_art_url:
+        "https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+    },
+    {
+      name: "The Gun 1",
+      artist: "Lorn",
+      album: "Ask The Dust",
+      url: "https://521dimensions.com/song/08 The Gun.mp3",
+      cover_art_url:
         "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
-    }
+    },
+    {
+      name: "The Gun 2",
+      artist: "Lorn",
+      album: "Ask The Dust",
+      url: "https://521dimensions.com/song/08 The Gun.mp3",
+      cover_art_url:
+        "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
+    },
+    {
+      name: "The Gun 3",
+      artist: "Lorn",
+      album: "Ask The Dust",
+      url: "https://521dimensions.com/song/08 The Gun.mp3",
+      cover_art_url:
+        "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
+    },
   ],
 
   playlists: {
     recent_work: {
-      songs: [0, 1],
+      songs: [0, 1, 2, 3, 4],
       title: "Best of Ancient Astronauts"
     }
   },
