414ba01edd143ee0299704a0c836b0fe01cb3e23
Abel <abel.hancock@liferay.com>
2019-08-22 23:27:41 -0700

    update playlists

--- init.js
+++ init.js
@@ -3,49 +3,69 @@ Amplitude.init({
     {
       name: "JFTP",
       artist: "Abel Hancock",
-      album: "Voice Overs",
+      album: "Short Film",
       url:
-        "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/jftp.mp3",
+        "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/short-film/jftp.mp3",
       cover_art_url:
         "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/jftp-artwork.jpg"
     },
     {
       name: "Old Leviathan",
       artist: "Abel Hancock",
-      album: "Voice Overs",
-      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/jftp-2.mp3",
+      album: "Short Film",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/short-film/jftp-2.mp3",
       cover_art_url:
         "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/old-leviathan.jpg"
     },
     {
       name: "Show Opener 1",
       artist: "Abel Hancock",
-      album: "Trailers & Openers",
-      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/compel-opener-2016.mp3",
+      album: "Show Opener",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/show-opener/compel-opener-2016.mp3",
       cover_art_url:
         "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/compel-opener-2016.jpg"
     },
     {
       name: "Show Opener 2",
       artist: "Abel Hancock",
-      album: "Trailers & Openers",
-      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/compel-opener-2017.mp3",
+      album: "Show Opener",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/show-opener/compel-opener-2017.mp3",
       cover_art_url:
         "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/compel-opener-2017.jpg"
     },
     {
       name: "Now. I. See.",
       artist: "Abel Hancock",
-      album: "Voice Over",
-      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/now-i-see.mp3",
+      album: "Short Film",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/short-film/now-i-see.mp3",
       cover_art_url:
         "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/now-i-see.jpg"
+    },
+    {
+      name: "War & Pain Teaser",
+      artist: "Abel Hancock",
+      album: "Trailers",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/trailers/war-and-pain-teaser.mp3",
+      cover_art_url:
+        "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/war-and-pain-teaser.jpg"
+    },
+    {
+      name: "War & Pain Trailer",
+      artist: "Abel Hancock",
+      album: "Trailers",
+      url: "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/audio/trailers/war-and-pain.mp3",
+      cover_art_url:
+        "https://raw.githubusercontent.com/abelhancock/recent-work-playlist/master/images/war-and-pain.jpg"
     },
   ],
 
   playlists: {
-    recent_work: {
+    short_film: {
       songs: [0, 1, 2, 3, 4],
+      title: "Recent Work"
+    },
+    trailers: {
+      songs: [5,6],
       title: "Recent Work"
     }
   },
